d5e4f6bffef4b9ae4e5dbfb57ae0ea7bad061b4c
Paulina <paulina.recko@gmail.com>
2019-01-18 21:08:18 +0100

    Add model and nested form for ingredients

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require cocoon
 //= require jquery3
 //= require popper
 //= require bootstrap
+//= require cocoon
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_174924) do
+ActiveRecord::Schema.define(version: 2019_01_18_150636) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
